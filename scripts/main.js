e83674339b0fd7e842e44cae082a13c6187ad1a0
Renzperi <inandanrenzperi09@gmail.com>
2023-12-15 16:43:20 +0800

    Dropdown menu has been added

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
 window.onload = function(){
-  
+	// dropdown menu
+	const dropDownMenuContainer = document.querySelector(".dropdownMenu");
+	const dropdownBtn = document.querySelector(".dropdownMenuNav button");
+	dropDownMenuContainer.innerHTML = document.querySelector(".MenuNav").innerHTML; //dynamically enters content of dropdown menu
+	
+	dropdownBtn.addEventListener("click", e => {
+		if(dropDownMenuContainer.style.display == "none"){
+			dropDownMenuContainer.style.display = "block";
+		}else{
+			dropDownMenuContainer.style.display = "none";
+		}
+	})
+	// end of dropdown menu
 }
