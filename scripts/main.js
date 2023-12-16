b7157a26b04d6acee813aba52771d96f8f12fd47
Renzperi Inandan <inandanrenzperi09@gmail.com>
2023-12-16 21:27:44 +0800

    header nav list centered and fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,4 @@
 window.onload = function(){
-	// // dropdown menu
-	// const dropDownMenuContainer = document.querySelector(".dropdownMenu");
-	// const dropdownBtn = document.querySelector(".dropdownMenuNav button");
-	// dropDownMenuContainer.innerHTML = document.querySelector(".MenuNav").innerHTML; //dynamically enters content of dropdown menu
-	
-	// dropdownBtn.addEventListener("click", e => {
-	// 	if(dropDownMenuContainer.style.visibility == "hidden"){
-	// 		dropDownMenuContainer.style.visibility = "visible";
-	// 	}else{
-	// 		dropDownMenuContainer.style.visibility = "hidden";
-	// 	}
-	// })
-	// // end of dropdown menu
-
 	// for canvas
 	const canvasContainer = document.querySelector(".canvasContainer");
 
