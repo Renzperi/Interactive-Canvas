aa79c8faf1e160d253f931da60607f43e0e005fb
Renzperi Inandan <inandanrenzperi09@gmail.com>
2023-12-16 20:40:26 +0800

    player object created with animation

--- scripts/main.js
+++ scripts/main.js
@@ -15,16 +15,47 @@ window.onload = function(){
 
 	// for canvas
 	const canvasContainer = document.querySelector(".canvasContainer");
-	const canvas = document.querySelector("#gameCanvas");
 
+	const canvas = document.querySelector("#gameCanvas");
 	const context = canvas.getContext("2d");
-	canvas.width = canvasContainer.offsetWidth;
+	canvas.width = canvasContainer.offsetWidth; 
 	canvas.height = canvasContainer.offsetHeight;
 
-	console.log(canvasContainer.getAttribute("maxWidth"));
-	console.log(canvasContainer.offsetWidth);
-	console.log(canvasContainer.offsetHeight);
+	// player character
+	function Player(x, y){
+		this.x = x;
+		this.y = y;
+		this.xVelocity = 1;
 
+		this.draw = function(){
+			context.fillStyle = "green";
+			context.fillRect(this.x, this.y, 20, 20);
+		}
+
+		this.update = function(){
+			if(this.x > canvas.width || this.x < 0){
+				this.xVelocity = -this.xVelocity;
+			}
+
+			this.x += this.xVelocity;
+
+			this.draw();
+		}
+	}
+	
+	let x = 10;
+	let y = canvas.height / 2;
+	const player1 = new Player(x, y);
+
+	function animate(){
+		requestAnimationFrame(animate);
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		player1.update();
+		console.log("hey");
+	}
+
+	animate();
 
 	// end of canvas
 }
